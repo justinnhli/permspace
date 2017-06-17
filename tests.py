eb683ca02239e7add741de70ec6ad0bd89d022d3
Justin Li <justinnhli@gmail.com>
2017-06-17 00:31:01 -0700

    add tests

--- tests.py
+++ tests.py
@@ -1,11 +1,46 @@
 from unittest import TestCase, main
 from itertools import product
 
-from permspace import PermutationSpace
+from permspace import Namespace, MixedRadix, PermutationSpace
+
+class NamespaceTest(TestCase):
+    def setUp(self):
+        key='test_key'
+        value=0
+        self.ns = Namespace(**{key:value})
+    def test_basic(self):
+        self.assertEqual(sorted(self.ns.keys()), ['test_key'])
+        self.assertEqual(self.ns.test_key, self.ns['test_key'])
+        self.assertEqual(self.ns['test_key'], 0)
+    def test_attribute_replace(self):
+        self.ns.test_key = 1
+        self.assertEqual(sorted(self.ns.keys()), ['test_key'])
+        self.assertEqual(self.ns.test_key, self.ns['test_key'])
+        self.assertEqual(self.ns['test_key'], 1)
+    def test_key_replace(self):
+        self.ns['test_key'] = 2
+        self.assertEqual(sorted(self.ns.keys()), ['test_key'])
+        self.assertEqual(self.ns.test_key, self.ns['test_key'])
+        self.assertEqual(self.ns['test_key'], 2)
+    def test_attribute_delete(self):
+        del self.ns.test_key
+        self.assertFalse(hasattr(self.ns, 'test_key'))
+        self.assertFalse('test_key' in self.ns)
+    def test_key_delete(self):
+        del self.ns['test_key']
+        self.assertFalse(hasattr(self.ns, 'test_key'))
+        self.assertFalse('test_key' in self.ns)
+
+class MixedRadixTest(TestCase):
+    def setUp(self):
+        self.init = [11, 3, 6, 23, 0, 59]
+        self.cycle = MixedRadix([12, 4, 7, 24, 60, 60], init_values=self.init)
+    def test_first(self):
+        self.assertEqual(next(self.cycle), self.init)
 
 class PermSpaceTest(TestCase):
     def setUp(self):
-        self.pspace = PermutationSpace(['arabic', 'letter_lower', 'roman_lower', 'question_name', 'constant'],
+        self.pspace = PermutationSpace(['arabic', 'letter_lower', 'roman_lower'],
             arabic=range(1, 4),
             letter_lower=list('abc'),
             roman_lower=['i', 'ii', 'iii'],
@@ -27,7 +62,7 @@ class PermSpaceTest(TestCase):
         with self.assertRaises(ValueError):
             self.pspace.add_filter((lambda undefined: undefined is None))
     def test_iter_from(self):
-        self.assertEqual(sum(1 for parameters in self.pspace.iter_from(arabic=2)), 18)
+        self.assertEqual(sum(1 for parameters in self.pspace.iter_from(Namespace(arabic=2))), 18)
     def test_ordering_errors(self):
         with self.assertRaises(ValueError):
             PermutationSpace(['arabic', 'arabic'],
@@ -42,6 +77,8 @@ class PermSpaceTest(TestCase):
                 arabic=range(1, 4),
                 letter_lower=list('abc'),
             )
+    def test_iter_only(self):
+        self.assertEqual(sum(1 for p in self.pspace.iter_only('letter_lower', 'b')), 3)
 
 if __name__ == '__main__':
     main()
