502f01ab8ac08eec17558318cbe935e48179c87e
Justin Li <justinnhli@gmail.com>
2019-01-11 14:30:50 -0800

    add minimal setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='permspace',
+    version='0.0.1',
+    description='A more powerful itertools.product',
+    url='https://github.com/justinnhli/permspace',
+    author='Justin Li',
+    author_email='justinnhli@gmail.com',
+    license='MIT',
+    packages=['permspace'],
+    zip_safe=False,
+)
