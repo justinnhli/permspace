df4d7a6100256a91ca6b19dcab749f91861f19d8
Justin Li <justinnhli@gmail.com>
2019-03-01 23:00:23 -0800

    bump version

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ from setuptools import setup
 
 setup(
     name='permspace',
-    version='0.0.1',
+    version='0.0.2',
     description='A more powerful itertools.product',
     url='https://github.com/justinnhli/permspace',
     author='Justin Li',
     author_email='justinnhli@gmail.com',
     license='MIT',
     packages=['permspace'],
-    zip_safe=False,
 )
