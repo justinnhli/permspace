62bcaaddab4ec434c4d0e30568042b44a460b528
Justin Li <justinnhli@gmail.com>
2019-03-06 21:33:15 -0800

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='permspace',
-    version='0.0.2',
+    version='0.0.3',
     description='A more powerful itertools.product',
     url='https://github.com/justinnhli/permspace',
     author='Justin Li',
